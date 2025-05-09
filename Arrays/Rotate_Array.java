6f8ef0fd66d3f0a7915d6219e22e767d7acdc452
Siddhi <121544611+siddhi3srivastava@users.noreply.github.com>
2025-05-09 13:05:20 +0530

    Solution to Rotate_Array.java

---
+++ Arrays/Rotate_Array.java
@@ -0,0 +1,26 @@
+/*Given an array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned change in the array in place.
+
+Note: Consider the array as circular.*/
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        int n=arr.length;
+        d=d%n;
+        
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+    }
+    
+    static void reverse(int arr[], int start, int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
